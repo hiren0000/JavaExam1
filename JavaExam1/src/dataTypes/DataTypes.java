ef5c380b760a6d17f896e06fbc74a9bbf0ab19dd
Hiren <Hiren@DESKTOP-Q4JDVS8>
2022-08-23 13:38:07 +0100

    FirstCommit into Exam

---
+++ JavaExam1/src/dataTypes/DataTypes.java
@@ -0,0 +1,29 @@
+package dataTypes;
+/*
+ * This is Example class
+ */
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		doSmt();
+		
+	}
+	/*
+	 * This method is showing the example of different dataTypes
+	 */
+	static void doSmt()
+	{
+		
+		int a = 5;
+		String name = "hiren";
+		double num = 10.08;
+		float number = 1.99f;
+		boolean b = true;
+		System.out.println(a+" "+name+" "+num+ " "+number+ " "+b);
+		int x = (byte)50;
+	}
+	
+
+}
